cdc004d5a8a969ca8ff2e4c5f9a55100407175c2
Vinzenz Prakoso <Vinzenz.Prakoso@campus.lmu.de>
2024-09-19 22:53:22 +0200

    Finished main.py and changed README.md
    Supports basic automatic sending of
    WhatsApp messages to a specific contact.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Lylyer-friendLY-daiLY-ExamReminder
+# Lylyer - friendLY daiLY Exam Reminder
+Automatic exam reminder for your friends
+## Usage
+1. Change RECIPIENT_NUMBER to your friends number
+2. Create a examName.txt file with the content of date (yyyy-mm-dd) for each exam (nothing else)
+3. Log into WhatsApp Web
+4. run `python main.py`
--- main.py
+++ main.py
@@ -1,4 +1,56 @@
 import pywhatkit
+import datetime
+import os
 
-# Send a WhatsApp Message to a Contact at 1:30 PM
-pywhatkit.sendwhatmsg("+Number", "Hi", 13, 30)
+# Phone Number of the Contact
+RECIPIENT_NUMBER = "+xxxxxxxxxxxxx"
+
+# main function
+def main():
+    # Foreach .txt file in the directory
+    directory = os.getcwd()
+    message = ""
+        
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".txt"): 
+            message += handle_txt_files(os.path.join(directory, filename))
+        else:
+            continue
+
+    message += "Viel Erfolg beim Lernen! (^-^)"
+    print(message)
+
+    # Send a WhatsApp Message to a Contact at 9:30 AM
+    try:
+        pywhatkit.sendwhatmsg(RECIPIENT_NUMBER, message, 22, 44)
+    except:
+        print("An Error Occurred!")
+
+
+# Function handle the .txt files to get the message
+def handle_txt_files(file):
+    message = ""
+
+    # Days until the Exam
+    date_of_exam = read_first_line(file)
+    filename = os.fsdecode(file)
+    name_of_exam = os.path.splitext(os.path.basename(filename))[0]
+    days = (datetime.datetime.strptime(date_of_exam, "%Y-%m-%d") - datetime.datetime.now()).days
+
+    # Message to be Sent
+    if days > 1:
+        message += "Noch " + days + " Tage bis zur " + name_of_exam + " Klausur!\n"
+    elif days == 1:
+        message += "Morgen ist die " + name_of_exam + " Klausur!\n"
+
+    return message
+
+# Function to Read First Line of File
+def read_first_line(file):
+    with open(file, 'r') as f:
+        first_line = f.readline()
+        return first_line
+
+if __name__ == "__main__":
+    main()
